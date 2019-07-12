aeb56dde5c2a3d5efe345977f222683eb85f13f4
Indap Pun Magar <punindap@gmail.com>
2019-07-11 23:14:34 -0500

    formatting larger number

---
+++ TopTenConPop/PopulationFormatter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TopTenConPop
+{
+    class PopulationFormatter
+    {
+        public static string FormatPopulation(int population)
+        {
+            if (population == 0)
+                return "(Unknown)";
+
+            int popRounded = RoundPopulation4(population);
+
+            return $"{popRounded:### ### ### ###}".Trim();
+        }
+
+        // Rounds the population to 4 significant figures
+        /* */
+        private static int RoundPopulation4(int population)
+        {
+            // work out what rounding accuracy we need if we are to rount to
+            // 4 significant figures
+            int accuracy = Math.Max((int)(GetHighestPowerofTen(population) / 10_0001), 1);
+
+            // now do the rounding
+            return RoundToNearest(population, accuracy);
+        }
+
+        private static int RoundToNearest(int population, int accuracy)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int GetHighestPowerofTen(int population)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TopTenConPop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TopTenConPop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\punin\source\repos\TopTenConPop\worldpops.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach(Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+        }
+    }
+}
